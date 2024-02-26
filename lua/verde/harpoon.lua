be3a579e3a9b9f1a96b34ac806f1bbefddb42d86
Nedeljko Radovanovic <nedeljko@obrizumgroup.com>
2024-02-26 01:16:08 +0100

    Basic development enabled (WIP)

---
+++ lua/verde/harpoon.lua
@@ -0,0 +1,38 @@
+local M = {
+  "ThePrimeagen/harpoon",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  }
+}
+
+function M.config()
+  -- set keymaps
+  local keymap = vim.keymap -- for conciseness
+
+  local mark = require("harpoon.mark")
+  local ui = require("harpoon.ui")
+
+  keymap.set("n", "<leader>ha", "<cmd>lua require('verde.harpoon').mark_file()<cr>")
+  keymap.set("n", "<leader>he", ui.toggle_quick_menu)
+
+  keymap.set("n", "<leader>hh", function()
+    ui.nav_file(1)
+  end)
+  keymap.set("n", "<leader>hj", function()
+    ui.nav_file(2)
+  end)
+  keymap.set("n", "<leader>hk", function()
+    ui.nav_file(3)
+  end)
+  keymap.set("n", "<leader>hl", function()
+    ui.nav_file(4)
+  end)
+end
+
+function M.mark_file()
+    require("harpoon.mark").add_file()
+    vim.notify "󱡅  marked file"
+end
+
+return M
---
+++ lua/verde/treesitter.lua
@@ -0,0 +1,15 @@
+local M = {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPost", "BufNewFile" },
+  build = ":TSUpdate",
+}
+
+function M.config()
+  require("nvim-treesitter.configs").setup {
+    ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python", "typescript", "javascript", "clojure", "rust", "dockerfile", "go", "html", "json", "php", "scss", "xml", "yaml", "css" },
+    highlight = { enable = true },
+    indent = { enable = true },
+  }
+end
+
+return M
