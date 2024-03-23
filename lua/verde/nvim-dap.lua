4bf8d80d737a74e4d84450880c1378980200910a
Nedeljko Radovanovic <ned@192.168.1.11>
2024-03-23 12:35:57 +0100

    Add nvim nio to nvim-dap-ui dependency object

--- lua/verde/nvim-dap.lua
+++ lua/verde/nvim-dap.lua
@@ -1,26 +1,27 @@
 local M = {
-	 "mfussenegger/nvim-dap",
+	"mfussenegger/nvim-dap",
 	event = "VeryLazy",
 	dependencies = {
 		{
-		"rcarriga/nvim-dap-ui",
-		config = function()
-			local dap, dapui = require("dap"), require("dapui")
-			dapui.setup()
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				dapui.close()
-			end
-			vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
-			vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-			vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
-		end,
-	},
+			"rcarriga/nvim-dap-ui",
+			dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+			config = function()
+				local dap, dapui = require("dap"), require("dapui")
+				dapui.setup()
+				dap.listeners.after.event_initialized["dapui_config"] = function()
+					dapui.open()
+				end
+				dap.listeners.before.event_terminated["dapui_config"] = function()
+					dapui.close()
+				end
+				dap.listeners.before.event_exited["dapui_config"] = function()
+					dapui.close()
+				end
+				vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+				vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+				vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+			end,
+		},
 		{ "theHamsta/nvim-dap-virtual-text" },
 	},
 }
