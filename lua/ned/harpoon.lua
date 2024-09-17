3982fb3c1f86584425b42e4caa1475e3705be0f8
Nedeljko Radovanovic <nradovanovic97@gmail.com>
2024-09-17 14:12:39 +0200

    adjust harpoon keybindings

--- lua/ned/harpoon.lua
+++ lua/ned/harpoon.lua
@@ -54,33 +54,61 @@ function M.config()
 			desc = "Harpoon quick menu",
 		},
 		{
-			"<leader>hh",
+			"<leader>h1",
 			function()
 				harpoon:list():select(1)
 			end,
 			desc = "Harpoon select 1",
 		},
 		{
-			"<leader>hj",
+			"<leader>h2",
 			function()
 				harpoon:list():select(2)
 			end,
 			desc = "Harpoon select 2",
 		},
 		{
-			"<leader>hk",
+			"<leader>h3",
 			function()
 				harpoon:list():select(3)
 			end,
 			desc = "Harpoon select 3",
 		},
 		{
-			"<leader>hl",
+			"<leader>h4",
 			function()
 				harpoon:list():select(4)
 			end,
 			desc = "Harpoon select 4",
-		}})
+		},
+{
+			"<leader>h5",
+			function()
+				harpoon:list():select(5)
+			end,
+			desc = "Harpoon select 5",
+		},
+{
+			"<leader>h6",
+			function()
+				harpoon:list():select(6)
+			end,
+			desc = "Harpoon select 6",
+		},{
+			"<leader>h7",
+			function()
+				harpoon:list():select(7)
+			end,
+			desc = "Harpoon select 7",
+		},{
+			"<leader>h8",
+			function()
+				harpoon:list():select(8)
+			end,
+			desc = "Harpoon select 8",
+		}
+
+	})
 end
 
 return M
