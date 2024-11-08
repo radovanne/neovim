9a05321efd40e958ee9b2203dac9b6c55b3cacc1
Nedeljko Radovanovic <nradovanovic97@gmail.com>
2024-11-08 19:56:18 +0100

    make colorscheme darker

--- lua/ned/colorscheme/rose-pine.lua
+++ lua/ned/colorscheme/rose-pine.lua
@@ -8,9 +8,12 @@ local M = {
 function M.config()
   local rose_pine_custom = {
     moon = {
-      base = "#262626", -- Lighter Dark Gray
-      surface = "#333333", -- Between Base and Overlay
-      overlay = "#3c3c3c", -- Lightened Overlay
+      -- base = "#262626", -- Lighter Dark Gray
+      -- surface = "#333333", -- Between Base and Overlay
+      -- overlay = "#3c3c3c", -- Lightened Overlay
+      base = "#161616",    -- Darker than #1f1f1f
+      surface = "#202020",  -- Darker than #292929
+      overlay = "#2a2a2a",  -- Darker than #333333
       text = "#D9D1E9", -- Touch of cold lavander
       love = "#C36E70",  -- Red-ish 
     },
