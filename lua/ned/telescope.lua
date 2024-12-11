4fde2310c46d645e732e898d483754373ff4c4a8
Nedeljko Radovanovic <nradovanovic97@gmail.com>
2024-12-11 18:39:41 +0100

    refactor(telescope): Add multi-grep function

--- lua/ned/telescope.lua
+++ lua/ned/telescope.lua
@@ -32,16 +32,72 @@ function M.config()
 	}
 
 	local wk = require("which-key")
-
-	require("telescope").load_extension("project")
 	local builtin = require("telescope.builtin")
+	local pickers = require("telescope.pickers")
+	local finders = require("telescope.finders")
+	local conf = require("telescope.config").values
+	local make_entry = require("telescope.make_entry")
+
+	local live_multigrep = function (opts)
+		opts = opts or {}
+		opts.cwd = opts.cwd or vim.uv.cwd()
+
+		local finder = finders.new_async_job {
+			command_generator = function (prompt)
+				if not prompt or prompt == "" then
+					return nil
+				end
+
+				local pieces = vim.split(prompt, "  ")
+				local args = { "rg" }
+
+				if pieces[1] then
+					table.insert(args, "-e")
+					table.insert(args, pieces[1])
+				end
+
+				if pieces[2] then
+					table.insert(args, "-g")
+					table.insert(args, pieces[2])
+				end
+
+---@diagnostic disable-next-line: deprecated
+				return vim.tbl_flatten {
+					args,
+					{ "--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case"}
+				}
+			end,
+
+			entry_maker = make_entry.gen_from_vimgrep(opts),
+			cwd = opts.cwd
+
+		}
+		pickers.new(opts, {
+			debounce = 100,
+			prompt_title = "Multi Grep",
+			finder = finder,
+			previewer = conf.grep_previewer(opts),
+			sorter = require("telescope.sorters").empty()
+		}):find()
+	end
 
 	wk.add({
 		{ "<leader>bb", "<cmd>Telescope buffers previewer=false<cr>", desc = "Find" },
 		{ "<leader>fb", builtin.git_branches, desc = "Checkout branch" },
 		{ "<leader>fc", builtin.colorscheme, desc = "Colorscheme" },
 		{ "<leader>ff", builtin.find_files, desc = "Find files" },
+		{ "<leader>fg", live_multigrep, desc = "Files Grep" },
 		{ "<leader>gf", builtin.git_files, desc = "Find git files" },
+		{ "<leader>en", function ()
+			builtin.find_files {
+				cwd = vim.fn.stdpath("config")
+			}
+		end, desc = "Edit Neovim" },
 		{
 			"<leader>pp",
 			"<cmd>lua require('telescope').extensions.project.project{ display_type='full' }<cr>",
@@ -59,25 +115,8 @@ function M.config()
 
 	require("telescope").setup({
 		defaults = {
-			file_ignore_patterns = base_ignore_patterns,
 			prompt_prefix = icons.ui.Telescope .. " ",
 			selection_caret = icons.ui.Forward .. " ",
-			entry_prefix = "   ",
-			initial_mode = "insert",
-			selection_strategy = "reset",
-			path_display = { "smart" },
-			color_devicons = true,
-			vimgrep_arguments = {
-				"rg",
-				"--color=never",
-				"--no-heading",
-				"--with-filename",
-				"--line-number",
-				"--column",
-				"--smart-case",
-				"--hidden",
-			},
-
 			mappings = {
 				i = {
 					["<C-n>"] = actions.cycle_history_next,
@@ -94,36 +133,18 @@ function M.config()
 				},
 			},
 		},
+		previewer = false,
+		theme = "ivy",
 		pickers = {
-			git_files = {
-				theme = "ivy",
-				previewer = false,
-			},
-			oldfiles = {
-				theme = "ivy",
-			},
 			current_buffer_fuzzy_find = {
-				theme = "ivy",
 				previewer = true,
 			},
-			live_grep = {
-				theme = "ivy",
-			},
-
-			grep_string = {
-				theme = "ivy",
-			},
-
 			find_files = {
-				theme = "ivy",
-				previewer = false,
+				file_ignore_patterns = base_ignore_patterns,
 				hidden = true,
-				find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+				find_command = { "rg", "--files", "--glob", "!**/.git/*" },
 			},
-
 			buffers = {
-				theme = "ivy",
-				previewer = false,
 				initial_mode = "normal",
 				mappings = {
 					i = {
@@ -134,35 +155,20 @@ function M.config()
 					},
 				},
 			},
-
-			planets = {
-				show_pluto = true,
-				show_moon = true,
-			},
-
 			colorscheme = {
-				theme = "ivy",
 				enable_preview = true,
 			},
-
 			lsp_references = {
-				theme = "ivy",
 				initial_mode = "normal",
 				enable_preview = true,
 			},
-
 			lsp_definitions = {
-				theme = "ivy",
 				initial_mode = "normal",
 			},
-
 			lsp_declarations = {
-				theme = "ivy",
 				initial_mode = "normal",
 			},
-
 			lsp_implementations = {
-				theme = "ivy",
 				initial_mode = "normal",
 			},
 		},
@@ -173,9 +179,11 @@ function M.config()
 				override_file_sorter = true, -- override the file sorter
 				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
 			},
-			-- theme = "ivy"
 		},
 	})
+
+	require("telescope").load_extension("project")
+	require('telescope').load_extension('fzf')
 end
 
 return M
